e9b4ef9d44d92f7a3f7b008c15015c628ec6a75b
Jiwon8037 <lego2090@naver.com>
2022-07-19 14:58:59 +0900

    로그인 구현

--- src/containers/auth/RegisterForm.jsx
+++ src/containers/auth/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import AuthForm from '../../components/auth/AuthForm';
@@ -6,6 +6,7 @@ import { changeField, initializeForm, register } from '../../modules/auth';
 import { check } from '../../modules/user';
 
 const RegisterForm = () => {
+    const [error,setError]=useState(null);
     const dispatch=useDispatch();
     const {form,auth,authError,user}=useSelector(({auth,user})=>({
         form:auth.register,
@@ -28,7 +29,14 @@ const RegisterForm = () => {
     const onSubmit=e=>{
         e.preventDefault();
         const {username,password,passwordConfirm}=form;
+        if([username,password,passwordConfirm].includes('')){
+            setError('빈칸을 모두 입력하세요');
+            return;
+        }
         if(password!==passwordConfirm){
+            setError('비밀번호가 일치하지 않습니다');
+            dispatch(changeField({form:'register',key:'password',value:''}));
+            dispatch(changeField({form:'register',key:'passwordConfirm',value:''}));
             return;
         }
         dispatch(register({username,password}));
@@ -40,7 +48,11 @@ const RegisterForm = () => {
 
     useEffect(()=>{
         if(authError){
-            console.log(authError);
+            if(authError.response.status===409){
+                setError('중복된 ID입니다');
+                return;
+            }
+            setError('회원가입 실패!')
             return;
         }
         if(auth){
@@ -59,7 +71,7 @@ const RegisterForm = () => {
     },[user])
     
     return (
-        <AuthForm type='register' form={form} onChange={onChange} onSubmit={onSubmit}/>
+        <AuthForm type='register' form={form} onChange={onChange} onSubmit={onSubmit} error={error}/>
     );
 };
 
